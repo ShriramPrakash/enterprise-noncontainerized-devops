4a2c30c1edc28b24b92a41bd72a6706be16a88b2
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-03-20 13:31:09 +0530

    Update main.tf

---
+++ terraform/modules/security-groups/main.tf
@@ -0,0 +1,65 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.name}-alb-sg"
+  description = "ALB security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "linux_web_sg" {
+  name        = "${var.name}-linux-web-sg"
+  description = "Linux web SG"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "linux_agent_sg" {
+  name        = "${var.name}-linux-agent-sg"
+  description = "Linux agent SG"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "windows_agent_sg" {
+  name        = "${var.name}-windows-agent-sg"
+  description = "Windows agent SG"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
