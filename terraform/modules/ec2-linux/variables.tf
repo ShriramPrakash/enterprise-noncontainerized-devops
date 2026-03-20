3c1666fdf5c4a10aa9eff47bbf86d9a44151c72f
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-03-20 13:33:59 +0530

    Update variables.tf

---
+++ terraform/modules/ec2-linux/variables.tf
@@ -0,0 +1,6 @@
+variable "name" { type = string }
+variable "ami_id" { type = string }
+variable "instance_type" { type = string }
+variable "subnet_id" { type = string }
+variable "security_group_id" { type = string }
+variable "instance_profile_name" { type = string }
