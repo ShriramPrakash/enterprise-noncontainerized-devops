946fba4054a5e764a5adf6d791a48c68afb20563
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-03-20 13:34:23 +0530

    Update main.tf

---
+++ terraform/modules/ec2-linux/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  iam_instance_profile   = var.instance_profile_name
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted   = true
+    volume_size = 20
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
