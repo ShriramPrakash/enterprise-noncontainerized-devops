a7bddc4a3cea54c340656b9018288e602d112d74
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-03-20 13:25:31 +0530

    Update variables.tf

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+variable "name" { type = string }
+variable "vpc_cidr" { type = string }
+variable "public_subnet_1_cidr" { type = string }
+variable "public_subnet_2_cidr" { type = string }
+variable "az1" { type = string }
+variable "az2" { type = string }
